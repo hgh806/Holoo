8b9823a1c4cd60719341d528ba97bcd4f6c6d749
Hossein806 <hghaffari806@gmail.com>
2024-10-09 08:12:38 +0330

    feat: bottom sheet with direction and bookmark button

--- app/src/main/java/com/holoo/map/ui/MainScreen.kt
+++ app/src/main/java/com/holoo/map/ui/MainScreen.kt
@@ -5,28 +5,45 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.res.ResourcesCompat
@@ -37,10 +54,12 @@ import com.carto.styles.PolygonStyleBuilder
 import com.carto.utils.BitmapUtils
 import com.holoo.map.R
 import com.holoo.map.ui.dialog.LocationInputDialog
+import com.holoo.map.ui.theme.HolooTheme
 import org.neshan.common.model.LatLng
 import org.neshan.mapsdk.MapView
 import org.neshan.mapsdk.model.Marker
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val context = LocalContext.current
@@ -57,30 +76,35 @@ fun MainScreen() {
         mutableStateOf(false)
     }
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                showInputDialog = true
-            }) {
-                Row(
-                    Modifier.padding(horizontal = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        modifier = Modifier
-                            .size(40.dp),
-                        imageVector = Icons.Rounded.Add,
-                        contentDescription = null
-                    )
-                    Text(
-                        stringResource(R.string.input_destination),
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                }
-            }
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(
+            initialValue = SheetValue.Expanded,
+            skipHiddenState = false
+        )
+    )
+
+    LaunchedEffect(key1 = marker) {
+        if (marker == null)
+            scaffoldState.bottomSheetState.hide()
+        else {
+            scaffoldState.bottomSheetState.show()
+            scaffoldState.bottomSheetState.expand()
         }
-    ) { paddings ->
-        Box(modifier = Modifier.padding(paddings)) {
+    }
+
+    BottomSheetScaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState,
+        containerColor = Color.Transparent,
+        sheetContainerColor = Color.Transparent,
+        sheetShadowElevation = 0.dp,
+        sheetSwipeEnabled = false,
+        sheetTonalElevation = 0.dp,
+        sheetContent = {
+            SheetContent(direction = {}, saveLocation = {})
+        }
+    ) {
+        Box(modifier = Modifier.padding()) {
             Box(
                 Modifier
                     .fillMaxSize()
@@ -120,6 +144,31 @@ fun MainScreen() {
                     )
                 }
             }
+
+            FloatingActionButton(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .statusBarsPadding(),
+                onClick = {
+                    showInputDialog = true
+                }
+            ) {
+                Row(
+                    Modifier.padding(horizontal = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(40.dp),
+                        imageVector = Icons.Rounded.Add,
+                        contentDescription = null
+                    )
+                    Text(
+                        stringResource(R.string.input_destination),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            }
         }
 
         if (showInputDialog) {
@@ -133,6 +182,71 @@ fun MainScreen() {
                 dismiss = { showInputDialog = false }
             )
         }
+    }
+}
+
+@Composable
+fun SheetContent(
+    direction: () -> Unit,
+    saveLocation: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .padding(bottom = 8.dp)
+            .padding(horizontal = 16.dp)
+            .navigationBarsPadding()
+            .height(72.dp)
+            .fillMaxWidth()
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(16.dp)
+            ),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Row(
+            Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = direction,
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_directions_24),
+                    contentDescription = stringResource(R.string.direction_button)
+                )
+
+                Text(stringResource(R.string.direction))
+            }
+
+            Button(
+                onClick = saveLocation,
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_bookmark_border_24),
+                    contentDescription = stringResource(R.string.save_button)
+                )
+
+                Text(stringResource(R.string.save))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewMainScreen() {
+    HolooTheme {
+        MainScreen()
     }
 }
 
